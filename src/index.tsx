2870846d50c9345846d87628660c7de376997afe
vladimirkhvan <vladimirkhvan23@gmail.com>
2022-07-18 13:16:25 +0500

    added react router

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+
 import './index.scss';
+
 import App from './App';
 
 const rootElement = document.getElementById('root');
 
 if (rootElement) {
     const root = ReactDOM.createRoot(rootElement);
-    root.render(<App />);
+    root.render(
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>,
+    );
 }
