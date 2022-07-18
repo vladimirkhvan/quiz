215cdfdd88a426ad841b85f2cc8c6862962d60cc
vladimirkhvan <vladimirkhvan23@gmail.com>
2022-07-18 21:34:46 +0500

    rewrote component to functional component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -8,17 +8,10 @@ interface PropsButton {
     link: string;
 }
 
-export class Button extends React.Component<PropsButton> {
-    placeholder: string;
-    link: string;
-
-    constructor(props: PropsButton) {
-        super(props);
-        this.placeholder = this.props.placeholder;
-        this.link = this.props.link
-    }
-
-    render(): React.ReactNode  {
-        return <Link to={this.link} className={styles.button}>{this.placeholder}</Link>;
-    }
-}
+export const Button: React.FC<PropsButton> = ({ placeholder, link }) => {
+    return (
+        <Link to={link} className={styles.button}>
+            {placeholder}
+        </Link>
+    );
+};
