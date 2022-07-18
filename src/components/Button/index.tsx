2490ad27c475b4ddce7f092ed3db5a28b935b3fb
vladimirkhvan <vladimirkhvan23@gmail.com>
2022-07-18 13:17:36 +0500

    added router

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,19 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 import styles from './Button.module.scss';
 
 interface PropsButton {
     placeholder: string;
+    link: string;
 }
 
 export class Button extends React.Component<PropsButton> {
     placeholder: string;
+    link: string;
 
-    constructor(props:PropsButton){
+    constructor(props: PropsButton) {
         super(props);
         this.placeholder = this.props.placeholder;
+        this.link = this.props.link
     }
 
-    render() {
-        return <button className={styles.button}>{this.placeholder}</button>;
+    render(): React.ReactNode  {
+        return <Link to={this.link} className={styles.button}>{this.placeholder}</Link>;
     }
 }
