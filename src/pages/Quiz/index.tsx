099ad9a15bf0f9a3c8165bb7228d528843532c5a
vladimirkhvan <vladimirkhvan23@gmail.com>
2022-07-19 12:13:50 +0500

    added quiz logic

--- src/pages/Quiz/index.tsx
+++ src/pages/Quiz/index.tsx
@@ -7,16 +7,39 @@ import { Button } from '../../components/Button';
 import { Question } from '../../components/Question';
 
 import { useAppSelector, useAppDispatch } from '../../redux/hooks';
+
 import { fetchQuestions } from '../../redux/Questions/asyncActions';
 import { Status } from '../../redux/Questions/types';
+import { completeQuiz, clearAnswers } from '../../redux/Answers/slice';
+
 import Skeleton from '../../components/Skeleton';
 
 export const Quiz: React.FC = () => {
     const dispatch = useAppDispatch();
     const { data: questions, status } = useAppSelector((state) => state.questions);
+    const { completingStatus, answers } = useAppSelector((state) => state.answers);
+
+    const [score, setScore] = React.useState(0);
+
+    const resultSign = score >= questions.length*0.8 ? ':S' : ':F';
+
+    function submitAnswers() {
+        let score: number = 0;
+        for (let i = 0; i < questions.length; i++) {
+            if (answers[questions[i].question] === questions[i].correct_answer) {
+                score++;
+            }
+        }
+        dispatch(completeQuiz());
+        setScore(score);
+    }
 
     React.useEffect(() => {
         dispatch(fetchQuestions());
+
+        return () => {
+            dispatch(clearAnswers());
+        };
     }, [dispatch]);
 
     return (
@@ -25,17 +48,23 @@ export const Quiz: React.FC = () => {
                 <Link to="/">
                     <span className={styles.arrow}>{'<-'}</span>
                 </Link>
-                <h2>good luck.</h2>
+                <h2>{completingStatus === 'completed' ? resultSign : 'good luck.'}</h2>
                 <time>10:00</time>
             </header>
 
+            {completingStatus === 'completed' && <h1 className={styles.score}>{score} / {questions.length}</h1>}
+
             <main className={styles.main}>
                 {status === Status.PENDING
-                    ? [...Array(4)].map((element, index) => <Skeleton key={index} />)
+                    ? [...Array(4)].map((elem, index) => <Skeleton key={index} />)
                     : questions.map((obj) => <Question key={obj.question} {...obj} />)}
             </main>
 
-            <Button placeholder="Submit" link="/" />
+            {completingStatus === 'completed' ? (
+                <Button placeholder="back" link="/" onClickHandler={submitAnswers} />
+            ) : (
+                <Button placeholder="Submit" link="" onClickHandler={submitAnswers} />
+            )}
         </div>
     );
 };
