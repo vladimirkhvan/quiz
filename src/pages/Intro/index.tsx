eb9dd3154793b422402dd739b276505b5a0cc233
vladimirkhvan <vladimirkhvan23@gmail.com>
2022-07-20 13:49:55 +0500

    reworked to FC / changed link

--- src/pages/Intro/index.tsx
+++ src/pages/Intro/index.tsx
@@ -3,15 +3,15 @@ import styles from './Intro.module.scss';
 
 import { Button } from '../../components/Button';
 
-export class Intro extends React.Component {
-    render(): React.ReactNode  {
-        return (
-            <div className={styles.intro}>
-                <h1 className={styles.title}>Quiz. What?</h1>
-                <p className={styles.description}>Quiz. A quiz is a bla-bla-bla.
-                <br/> good luck. no boring info</p>
-                <Button placeholder={"start."} link='quiz'/>
-            </div>
-        );
-    }
-}
+export const Intro = () => {
+    return (
+        <div className={styles.intro}>
+            <h1 className={styles.title}>Quiz. What?</h1>
+            <p className={styles.description}>
+                Quiz. A quiz is a bla-bla-bla.
+                <br /> good luck. no boring info
+            </p>
+            <Button placeholder={'start.'} link="filter" />
+        </div>
+    );
+};
