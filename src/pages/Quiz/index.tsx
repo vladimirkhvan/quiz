0ecf70fdfb9b4643b82a7df4b7e7539c88a68a73
vladimirkhvan <vladimirkhvan23@gmail.com>
2022-07-20 22:37:52 +0500

    added fallback / reset questions on exit

--- src/pages/Quiz/index.tsx
+++ src/pages/Quiz/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Navigate } from 'react-router-dom';
 
 import styles from './Quiz.module.scss';
 
@@ -12,13 +12,19 @@ import { fetchQuestions } from '../../redux/Questions/asyncActions';
 import { Status } from '../../redux/Questions/types';
 import { completeQuiz, clearAnswers } from '../../redux/Answers/slice';
 
+import { categoryDictionary } from '../../components/CategorySelector';
+
 import Skeleton from '../../components/Skeleton';
 import Timer from '../../components/Timer';
+import { resetQuestions } from '../../redux/Questions/slice';
 
 export const Quiz: React.FC = () => {
     const dispatch = useAppDispatch();
     const { data: questions, status } = useAppSelector((state) => state.questions);
     const { completingStatus, answers } = useAppSelector((state) => state.answers);
+    const { category, difficulty, type, numberOfQuestions } = useAppSelector(
+        (state) => state.filter,
+    );
 
     const [score, setScore] = React.useState(0);
 
@@ -36,11 +42,22 @@ export const Quiz: React.FC = () => {
     }
 
     React.useEffect(() => {
-        dispatch(fetchQuestions());
+        (async () => {
+            await dispatch(
+                fetchQuestions({
+                    category: categoryDictionary[category],
+                    difficulty,
+                    type,
+                    numberOfQuestions,
+                }),
+            );
+        })();
+
         return () => {
             dispatch(clearAnswers());
+            dispatch(resetQuestions());
         };
-    }, [dispatch]);
+    }, [dispatch, category, difficulty, type, numberOfQuestions]);
 
     return (
         <div className={styles.quiz}>
@@ -49,7 +66,11 @@ export const Quiz: React.FC = () => {
                     <span className={styles.arrow}>{'<-'}</span>
                 </Link>
                 <h2>{completingStatus === 'completed' ? resultSign : 'good luck.'}</h2>
-                {completingStatus === 'processing' ? <Timer time={60*10} /> : <time>0:00:00</time>}
+                {completingStatus === 'processing' ? (
+                    <Timer time={60 * 10} />
+                ) : (
+                    <time>0:00:00</time>
+                )}
             </header>
 
             {completingStatus === 'completed' && (
@@ -62,10 +83,13 @@ export const Quiz: React.FC = () => {
                 {status === Status.PENDING
                     ? [...Array(4)].map((elem, index) => <Skeleton key={index} />)
                     : questions.map((obj) => <Question key={obj.question} {...obj} />)}
+                {questions.length === 0 && status === Status.FULFILLED && (
+                    <Navigate to="/quizNotFound" replace={true} />
+                )}
             </main>
 
             {completingStatus === 'completed' ? (
-                <Button placeholder="back" link="/" onClickHandler={submitAnswers} />
+                <Button placeholder="go to main." link="/" onClickHandler={submitAnswers} />
             ) : (
                 <Button placeholder="Submit" link="" onClickHandler={submitAnswers} />
             )}
